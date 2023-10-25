023f68b60e0b245f28931527438e61496a9dcd8d
lee1nna <dlgkssk0209@gmail.com>
2023-10-25 23:34:41 +0900

    Add: Skills 이미지 추가

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,8 +8,9 @@ const Skills = () => {
         <div className={styles["skills-wrap"]}>
           <div className={styles["skill"]}>
             <div className={styles["skill-title"]}>Front-end</div>
-            <img src="/assets/html.png" height='90' alt="" />
-            <img src="/assets/css.png" height='90' alt="" />
+            <img src="/assets/html.png" height='80' alt="" />
+            <img src="/assets/css.png" height='80' alt="" />
+            <img src="/assets/js.png" height='80' alt="" />
             <img src="/assets/vue.png" height='80' alt="" />
             <img src="/assets/nuxt.png" height='50' alt="" />
             <img src="/assets/react.png" height='90' alt="" />
@@ -17,10 +18,14 @@ const Skills = () => {
           </div>
           <div className={styles["skill"]}>
           <div className={styles["skill-title"]}>Version Control</div>
+          <img src="/assets/github.png" height='90' alt="" />
+            <img src="/assets/git.png" height='45' alt="" />
           </div>
           <div className={styles["skill"]}>
-          <div className={styles["skill-title"]}>Version Control</div>
-          
+          <div className={styles["skill-title"]}>Etc</div>
+          <img src="/assets/firebase.png" height='50' alt="" style={{marginBottom:'20px'}}/>
+            <img src="/assets/strapi.png" height='40' alt="" style={{marginBottom:'20px'}}/>
+            <img src="/assets/react-native.png" height='35' alt="" />
           </div>
         </div>
       </div>
